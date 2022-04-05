39c9d7773f3892de2e62de87e0db1a360ac569bf
landry nahigombeye <nahigombeye.landry@elev.ga.ntig.se>
2022-04-05 14:38:27 +0200

    ntednhtd

---
+++ NOE_Lektionsuppgifter/NOE_Lektionsuppgifter/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+
+// string Hello; 
+// Hello = Console.ReadLine();
+
+
+//if (6 >= 3)
+//{    Console.WriteLine("Hello, World!");
+//}
+
+//Console.ReadLine();
+
+//
+
+//Console.WriteLine("Vad är ditt namn?");
+
+//string name = Console.ReadLine();
+
+//if (name != "noname")
+//{
+//Console.WriteLine("FEEEEL!");
+//}
+//else if (name == "noname")
+//{
+// Console.WriteLine("Welcome!");
+//}
+
+//Console.ReadLine();
+
+//
+
+//Console.WriteLine("Skriv namn och lösenord!");
+
+//string Landry = Console.ReadLine();
+//string pass = Console.ReadLine();
+
+//if (name != "noname" && pass != "nopass"){
+// Console.WriteLine("Vad är ditt namn?");
+//}
+
+//if (name == "noname"){
+// Console.WriteLine("Welcome!");
+//}
+
+//Console.ReadLine();
+
+//
+
+//for (int i = 0; i < 32; i++)
+//{
+// Console.WriteLine("Hello, world!");
+//}
+//Console.ReadLine();
+//
+
+//string password = "";
+
+//while (password != "No pass")
+//{
+//  Console.WriteLine("skriv lösenord:");
+//   password = Console.ReadLine();
+//}
+
+//Console.WriteLine("grattis!");
+
+//Console.ReadLine();//
+
+
+
+
+//for (int i = 0; i < 5; i++)
+//{
+//Console.WriteLine("Skriv ett tal:");
+//string tal = Console.ReadLine();
+//int resultat;
+//bool lyckad = int.TryParse(tal, out resultat);
+
+//if (resultat > 5)
+// Console.WriteLine("Högre än 5!");
+
+// Console.WriteLine(tal * 4);
+// }
+// {
+//   Console.WriteLine(i);
+//   i++;
+// }
+// while (i > 5);
+
+// Console.WriteLine("Högre än 5!");
+//
+
+/*Console.WriteLine("Gissa en siffra!");
+Console.ReadLine();
+
+Random Siffra = new Random();
+
+int tal = Siffra.Next(10);
+tal = Siffra.Next(0, 10);
+
+int gissning = -1;
+while (gissning != tal)
+{
+    Console.WriteLine("Skriv en siffra:");
+    string giss = Console.ReadLine();
+    bool lyckad = int.TryParse(giss, out gissning);
+    if (gissning < 5)
+    {
+        Console.WriteLine("Gissning för hög");
+        Console.ReadLine();
+    }
+    else if (gissning > 5)
+    {
+        Console.WriteLine("Gissning för låg");
+        Console.ReadLine();
+    }
+    else
+    {
+       Console.WriteLine("RÄTT SVAR!");
+       Console.ReadLine();
+    }
+
+}
+
+Console.ReadLine();*/
